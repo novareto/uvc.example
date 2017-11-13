afb3953e69a530a113b37a85403a8da35bd76561
goschtl <ck@novareto.de>
2017-11-13 11:14:42 +0100

    Initial Import

---
+++ uvc/example/README.txt
@@ -0,0 +1,10 @@
+=======
+Doctest
+=======
+
+uvc.example
+
+:Test-Layer: functional
+
+   >>> 1 + 1
+   2
---
+++ uvc/example/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# # Copyright (c) 2007-2017 NovaReto GmbH
+# # cklinger@novareto.de
+
+
+import grok
+import uvcsite
+
+from time import sleep
+from zope import interface
+
+from uvc.tbskin.resources import TBSkinViewlet
+
+
+class TBSkinViewlet(TBSkinViewlet):
+    pass
+
+
+class Index(uvcsite.Page):
+    grok.context(interface.Interface)
+
+
+    def render(self):
+        return u" HALLO WELT"
